9026f525ed01241d6d5d790a6dca4ec1f487053e
Envy <sungin0406@naver.com>
2024-03-18 15:29:50 +0900

    spring 초기 설정

---
+++ ExpertSpring30/src/test/java/com/example/demo/ExpertSpring30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertSpring30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
