a9dbd5ce9d2fcbdead89b4316119764db9848817
Envy <sungin0406@naver.com>
2024-03-19 18:49:29 +0900

    Spring AOP - 횡단 관심사와 핵심 관심사

---
+++ ExpertSpring30/src/main/java/aop001/Girl.java
@@ -0,0 +1,18 @@
+package aop001;
+
+public class Girl {
+	public void runSomething() {
+		System.out.println("열쇠로 문을 열고 집에 들어감.");
+		try {
+			System.out.println("요리를 한다."); //핵심 관심사
+		}catch(Exception e) {
+			if(e.getMessage().equals("집에 불남")) {
+				System.out.println("119에 신고해");
+			}
+		}finally {
+			System.out.println("불끄고 잔다.");
+		}
+		
+		System.out.println("문 잠그고 집을 나선다.");
+	}
+}
---
+++ ExpertSpring30/src/main/java/aop001/Start.java
@@ -0,0 +1,13 @@
+package aop001;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Boy sungin = new Boy();
+		Girl who = new Girl();
+		
+		sungin.runSomething();
+		who.runSomething();
+	}
+
+}
